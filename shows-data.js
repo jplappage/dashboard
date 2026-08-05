70459f368e92cdcf4f0ad805a08958807df9a9a1
JP <jplappage@outlook.com>
2026-08-05 22:28:31 +0100

    update 05/08/2026 22:28:31.24

--- shows-data.js
+++ shows-data.js
@@ -24,7 +24,7 @@ const SHOWS = [
   {name:"Severance",                             status:"ongoing",  aired:2, next:"S3 · est. Summer 2027", recheck:"2026-08-07", imdbId:"tt11280740"},
   {name:"Rings of Power",                        status:"ongoing",  aired:2, next:"S3 · 11 Nov 2026", imdbId:"tt7631058"},
   {name:"House of the Dragon",                   status:"ongoing",  aired:3, next:"S4 · 2028", recheck:"2026-08-07", airing:"S3 · Mondays", airingUntil:"2026-08-10", imdbId:"tt11198330"},
-  {name:"Ted Lasso",                             status:"ongoing",  aired:3, next:"S4 · 5 Aug 2026", imdbId:"tt10986410"},
+  {name:"Ted Lasso",                             status:"ongoing",  aired:4, next:null, recheck:"2026-08-14", airing:"S4 · Wednesdays", airingUntil:"2026-10-07", imdbId:"tt10986410"},
   {name:"Ted",                                   status:"finished", aired:2, imdbId:"tt14824792"},
   {name:"Tires",                                 status:"ongoing",  aired:2, next:"S3 · 13 Aug 2026", imdbId:"tt31491435"},
   {name:"The Boys",                              status:"finished", aired:5, next:null, imdbId:"tt1190634"},
